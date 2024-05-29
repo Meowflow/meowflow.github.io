977e976e76ed1100502c99f29f925bda73fbd3b0
Flow is best <64538953+Meowflow@users.noreply.github.com>
2024-05-29 21:10:23 +0000

    Added BTD5 and Eaglercraft 1.8.8!

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // thanks chatgpt
 
 // Array of text options
-var textArray = ['Did you know? meowflow.neocities.org, one of my websites, has over 200 thousand views! Wow!', 'Did you know? I used ChatGPT to make the code to shuffle this text after every reload!', 'Did you know? I hate IXL cuz I swear whenever im at 99 smartscore i get a question wrong and then im at 73', 'Did you know? This website was hosted using porkbun, github, and replit!', 'Did you know? My discord is "meowflow". Add me if you want to request a website :D', 'Did you know? 1+1 actually equals 3821907518942193, according to Harvard University!']; // Add more text options as needed
+var textArray = ['Did you know? meowflow.neocities.org, one of my websites, has over 200 thousand views! Wow!', 'Did you know? I used ChatGPT to make the code to shuffle this text after every reload!', 'Did you know? I hate IXL cuz I swear whenever im at 99 smartscore i get a question wrong and then im at 73', 'Did you know? This website was hosted using porkbun, github, and replit!', 'Did you know? My discord is "meowflow". Add me if you want to request a website :D', 'Did you know? 1+1 actually equals 3821907518942193, according to Harvard University!', 'Your favorite educational website!']; // Add more text options as needed
 
 // Function to return a random item from an array
 function getRandomText(arr) {
